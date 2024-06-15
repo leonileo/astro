682f7d25e4aceec0ece5408c19b56c42b15280a9
leonileo <leolittleprogrammer@gmail.com>
2024-06-15 17:47:37 +0300

    Edited: text

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
         <div className='w-full h-[100vh] bg-gradient-to-t bg-lime-50 flex justify-center items-center'>
-            <p className='font-extralight text-[50px]'> This is a test website! </p>
+            <p className='font-extralight text-[50px] text-center'> This is a test website! </p>
         </div>
     </>
   )
