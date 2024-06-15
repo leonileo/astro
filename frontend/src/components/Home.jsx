ec033e3bdabecbbb0899e5c8c1ae198402bae740
leonileo <leolittleprogrammer@gmail.com>
2024-06-15 19:18:17 +0300

    Change: change the btn functionality from onclick to using useEffect to fetch on page refresh

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,21 +1,23 @@
 import axios from "axios"
 import { motion } from "framer-motion"
 import Message from "./Message"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Home = () => {
   const [success, setSuccess] = useState(false);
   const [mess, setMess] = useState("");
 
-  const getHello = () => {
-    axios.get('https://astro-backend-yj6z.onrender.com/')
-    .then(mess => {
-      setSuccess(true)
-      setMess(mess.data)
-    }).catch(()=>{
-      setSuccess(false)
-    })
-  }
+  // const getHello = () => {
+  // }
+    useEffect(() => {
+      axios.get('https://astro-backend-yj6z.onrender.com/')
+      .then(mess => {
+        setSuccess(true)
+        setMess(mess.data)
+      }).catch(()=>{
+        setSuccess(false)
+      })
+    },[])
   return (
       <div 
         //   initial={{ x: "10%", opacity:0 }}
@@ -26,7 +28,7 @@ const Home = () => {
         <div className=' grid justify-center'>
           <p className='font-extralight text-[50px] text-center'> This is a test website! </p>
           <p>Edited now!</p>
-          {success && <><Message mess={mess}/></>}
+          <Message mess={mess}/>
           <div className='my-5 text-center'>
             <button 
             // initial={{y: "-100%", opacity:0}} 
